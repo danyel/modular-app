ca4fd5ad593c0a0132f73aa33650f1a578a1075a
danyel <daniel.noulet@gmail.com>
2015-05-22 22:04:20 +0200

    remove unnecessary variables

--- core/be.urpi.software.modular.core.watcher/src/main/java/be/urpi/software/modular/core/watcher/directory/ThreadDirectoryWatcher.java
+++ core/be.urpi.software.modular.core.watcher/src/main/java/be/urpi/software/modular/core/watcher/directory/ThreadDirectoryWatcher.java
@@ -3,11 +3,7 @@ package be.urpi.software.modular.core.watcher.directory;
 import be.urpi.software.modular.core.watcher.AbstractThreadWatcher;
 import be.urpi.software.modular.core.watcher.WatchAbleException;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 public class ThreadDirectoryWatcher extends AbstractThreadWatcher<DirectoryWatchAble> implements DirectoryWatcher {
-    private AtomicBoolean stop = new AtomicBoolean(false);
-
     public ThreadDirectoryWatcher(final DirectoryWatchAble directoryWatchAble) throws WatchAbleException {
         super(directoryWatchAble);
     }
